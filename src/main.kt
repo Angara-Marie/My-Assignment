a534f2c573e04b46018ca0edd6788a8175a37486
Angara-Marie <angaramarie@gmail.com>
2022-02-19 16:44:16 +0300

    My Assignment

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    name()
+    modulus(225, 14)
+    add(23, 456, 67, 44)
+    fact()
+
+
+}
+fun name(){
+    var name = "Tessa"
+    println("Hello " + name)
+}
+fun modulus(a:Int, b:Int){
+    var modulus = a % b
+    println(modulus)
+}
+fun add(a:Int, b:Int, c:Int, d:Int){
+    var add = (a+b+c+d)
+    println(add)
+
+}
+fun fact(){
+    var fact= "I love food"
+    println(fact)
+}
+
+
